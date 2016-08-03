7707dd4b9c1aa820216da1ac0c4561b1797d1d75
Bhaskar Tejaswi <bhaskartejaswi2008@gmail.com>
2016-08-03 23:01:56 +0530

    Update jsonParser.java

--- jsonParser.java
+++ jsonParser.java
@@ -4,34 +4,40 @@ import java.util.HashMap;
  * @author Bhaskar
  */
 import java.io.*;
+
 public class jsonParser {
+
     String json = "";
     HashMap map = new HashMap(); //present in util package
-    public void setJson(String json){
+
+    public void setJson(String json) {
         this.json = json;
-        json = json.substring(1,json.length()-1);
+        json = json.substring(1, json.length() - 1);
         String[] arr = this.json.split(",");
-        for(String m : arr){
-            String[]field = m.split(":");
-            map.put(field[0],field[1]);
+        for (String m : arr) {
+            String[] field = m.split(":");
+            map.put(field[0], field[1]);
         }
     }
 
-    public String getField(String field){
-        return (String)map.get(field);
+    public String getField(String field) {
+        return (String) map.get(field);
     }
-    
-    public void SetField(String field,String value){
-         map.put(field,value);
+
+    public void SetField(String field, String value) {
+        map.put(field, value);
     }
-    
-    
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         String x = "{\"Sens\":\"Tem\",\"adr\":1,\"dta\":[36.0],\"typ\":\"00\",\"UId\":68,\"LId\":\"Hn4\"}";
         jsonParser jp = new jsonParser();
         jp.setJson(x);
+        String field = "typ";
+        float Temp = 38.9f;
+        System.out.println(jp.getField("\"typ\""));
+        jp.SetField("\"" + field + "\"", "\""+Temp + "\"");
         System.out.println(jp.getField("\"typ\""));
     }
 }
