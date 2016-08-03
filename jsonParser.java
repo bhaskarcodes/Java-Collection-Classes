5fc72f08da8863b2e2a6c084cfea6541812b464a
Bhaskar Tejaswi <bhaskartejaswi2008@gmail.com>
2016-08-03 22:20:32 +0530

    Add files via upload

---
+++ jsonParser.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+/**
+ *
+ * @author Bhaskar
+ */
+import java.io.*;
+public class jsonParser {
+    String json = "";
+    HashMap map = new HashMap(); //present in util package
+    public void setJson(String json){
+        this.json = json;
+        json = json.substring(1,json.length()-1);
+        String[] arr = this.json.split(",");
+        for(String m : arr){
+            String[]field = m.split(":");
+            map.put(field[0],field[1]);
+        }
+    }
+
+    public String getField(String field){
+        return (String)map.get(field);
+    }
+    
+    public void SetField(String field,String value){
+         map.put(field,value);
+    }
+    
+    
+    public static void main(String[]args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String x = "{\"Sens\":\"Tem\",\"adr\":1,\"dta\":[36.0],\"typ\":\"00\",\"UId\":68,\"LId\":\"Hn4\"}";
+        jsonParser jp = new jsonParser();
+        jp.setJson(x);
+        System.out.println(jp.getField("\"typ\""));
+    }
+}
